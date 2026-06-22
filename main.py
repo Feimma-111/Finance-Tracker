c8970590cbf3879b3278e82dfd0fc8e02f4918a1
Peter Guan <peteryiting@gmail.com>
2026-06-22 15:20:27 +0800

    Initiated and created a rudimentary version of a finance tracker

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Hello and this is the start of my finance tracker")
+run = True
+total_transactions = []
+while run :
+    action = input("If you would like to enter a new transaction, press t" \
+           " if you would like to see your portfolio, press p, to end the program, press n: ")
+    if action.lower() == "t" :
+        transaction = input("Please enter what your transaction was: ")
+        amount = input("Please enter the amount of the transaction: ")
+        category = input("Please enter the category of spending: ")
+        transaction_date = input("Please enter the date of transaction: ")
+        num_amount = float(amount)
+        current_transaction = dict(name=transaction, price=num_amount, type=category, date=transaction_date)
+        total_transactions.append(current_transaction)
+    elif action.lower() == "p" :
+        total_spent = 0
+        for item in total_transactions:
+            total_spent += item.get("price")
+        print(total_spent)
+    else :
+        run = False
+
+
