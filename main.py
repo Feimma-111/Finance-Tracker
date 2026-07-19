374c3fdab7f3b76b3585c4c1a3f441f11b66d2dc
Peter Guan <peteryiting@gmail.com>
2026-07-19 20:32:33 +0800

    date and datetime created

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 import os
+import datetime
 
-print("Hello and this is the start of my finance tracker")
+print("Hello and welcome to my finance tracker")
 run = True
 total_transactions = []
 file_exists = os.path.exists('information_save_file')
@@ -12,16 +13,18 @@ if file_exists:
             total_transactions.append(saved_transaction)
 
 while run :
-    action = input("If you would like to enter a new transaction, press t" \
-           " if you would like to see your portfolio, press p, to save your portfolio, press s" \
-           " to end the program, press n: ")
+    action = input("Enter [p] if you would like to see your portfolio, enter [t] if you would " \
+           "like to enter a new transaction, enter [s] to save your current portfolio, and " \
+           "enter [n] to end the program: ")
     if action.lower() == "t" :
         transaction = input("Please enter what your transaction was: ")
         amount = input("Please enter the amount of the transaction: ")
         category = input("Please enter the category of spending: ")
-        transaction_date = input("Please enter the date of transaction: ")
+        transaction_date = input("Please enter the date of transaction in the format YYYY/MM/DD: ")
+        date_format = '%Y/%m/%d'
+        true_date = datetime.datetime.strptime(transaction_date, date_format)
         num_amount = float(amount)
-        current_transaction = dict(name=transaction, price=num_amount, type=category, date=transaction_date)
+        current_transaction = dict(name=transaction, price=num_amount, type=category, date=true_date.isoformat())
         total_transactions.append(current_transaction)
     elif action.lower() == "p" :
         total_spent = 0
